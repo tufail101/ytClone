64f27dcd9edc1df14cd73c3b8e8333041abb36af
tufail101 <modreneducation479@gmail.com>
2025-06-26 23:25:44 +0530

    ..

--- server/src/routers/comment.routers.js
+++ server/src/routers/comment.routers.js
@@ -1,16 +1,21 @@
-import express, { Router } from "express"
-import { deleteComment, getVideoComment, publishCommentOnComment, publishCommentOnVideo } from "../controllers/comment.controllers.js"
-import { varifyjwt } from "../middlewares/auth.middlewares.js"
+import express, { Router } from "express";
+import {
+  deleteComment,
+  getVideoComment,
+  publishCommentOnComment,
+  publishCommentOnVideo,
+} from "../controllers/comment.controllers.js";
+import { varifyjwt } from "../middlewares/auth.middlewares.js";
 
+const router = express.Router();
 
-const router = express.Router()
+router.use(varifyjwt);
 
-router.use(varifyjwt)
+router.route("/postVideoCommnet/:videoId").post(publishCommentOnVideo);
+router.route("/getVideoComment/:videoId").get(getVideoComment);
+router
+  .route("/publishCommentOnComment/:commentId")
+  .post(publishCommentOnComment);
+router.route("/delete/:commentId").patch(deleteComment);
 
-router.route("/postVideoCommnet/:videoId").post(publishCommentOnVideo)
-router.route("/getVideoComment/:videoId").get(getVideoComment)
-router.route("/publishCommentOnComment/:commentId").post(publishCommentOnComment)
-router.route("/delete/:commentId").patch(deleteComment)
-
-
-export default router
+export default router;
